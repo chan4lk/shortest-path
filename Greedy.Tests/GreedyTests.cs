5a8ab817aa469c117cedae8d0df173c564eb8d34
Chandima Ranaweera <chan4lk@gmail.com>
2018-09-02 06:18:01 +0530

    initail commit

---
+++ Greedy.Tests/GreedyTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Greedy.Tests
+{
+    [TestFixture]
+    public class GreedyTests
+    {
+        [Test]
+        public void GetLengthByCordsFromRootTest()
+        {
+            ShortestPath path = new ShortestPath();
+            Point root = new Point(0, 0);
+            Point point = new Point(3, 4);
+            
+            var length = path.CalculateLength(root, point);
+            Assert.That(length, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void GetLengthByCordsTest()
+        {
+            ShortestPath path = new ShortestPath();
+            Point root = new Point(2, 3);
+            Point point = new Point(3, 4);
+
+            var length = path.CalculateLength(root, point);
+            Assert.That(length, Is.EqualTo(Math.Sqrt(2)));
+        }
+
+        [Test]
+        public void CalculateZeroCitiesLength()
+        {
+            ShortestPath path = new ShortestPath();            
+
+            double length = path.CalculatePerimeter(new List<Point>());
+            Assert.That(length, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateSingleCitiesLength()
+        {
+            ShortestPath path = new ShortestPath();
+            Point root = new Point(2, 3);
+            double length = path.CalculatePerimeter(new List<Point>() { root });
+            Assert.That(length, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculateTwoCitiesLength()
+        {
+            ShortestPath path = new ShortestPath();
+            Point city1 = new Point(0, 0);
+            Point city2 = new Point(3, 4);
+            double length = path.CalculatePerimeter(new List<Point>() { city1, city2 });
+            Assert.That(length, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void CalculateThreeCitiesLength()
+        {
+            ShortestPath path = new ShortestPath();
+            Point city1 = new Point(0, 0);
+            Point city2 = new Point(2, 3);
+            Point city3 = new Point(3, 4);
+            double length = path.CalculatePerimeter(new List<Point>() { city1,city2, city3 });
+            Assert.That(length, Is.EqualTo(Math.Sqrt(13) + Math.Sqrt(2) + 5));
+        }
+    }
+}
---
+++ Greedy/ShortestPath.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy
+{
+    public class ShortestPath
+    {
+        /*
+         * 5
+            0,0
+            7,2
+            10,7
+            8,20
+            4,30
+         * 
+         */
+        public double CalculateLength(Point root, Point point)
+        {
+            return Math.Sqrt(Math.Pow(Math.Abs(root.X - point.X), 2) + Math.Pow(Math.Abs(root.Y - point.Y), 2));
+        }
+
+        public double CalculatePerimeter(List<Point> points)
+        {
+            double perimeter = 0;
+            if (points.Count < 2)
+            {
+                perimeter = 0;
+            }
+            else
+            {
+                var count = points.Count;
+                for (int i = 0; i < count - 1; i++)
+                {
+                    perimeter += CalculateLength(points[i], points[i + 1]);
+                }
+                if (count > 2)
+                    perimeter += CalculateLength(points[count - 1], points[0]);
+            }
+
+            return perimeter;
+
+        }
+    }
+}
